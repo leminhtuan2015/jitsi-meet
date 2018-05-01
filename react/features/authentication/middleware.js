3be2ebefcf44d555cb8f0773108d401858f52bcc
paweldomas <pawel.domas@jitsi.org>
2018-05-01 14:58:46 -0500

    fix(auth/external API): CONFERENCE_FAILED with login dialog
    Delay the CONFERENCE_FAILED event until the user cancels the login
    dialog using the 'recoverable' event flag.

--- react/features/authentication/middleware.js
+++ react/features/authentication/middleware.js
@@ -108,9 +108,12 @@ MiddlewareRegistry.register(store => next => action => {
     case CONNECTION_FAILED: {
         const { error } = action;
 
-        error
+        if (error
             && error.name === JitsiConnectionErrors.PASSWORD_REQUIRED
-            && store.dispatch(_openLoginDialog());
+            && typeof error.recoverable === 'undefined') {
+            error.recoverable = true;
+            store.dispatch(_openLoginDialog());
+        }
         break;
     }
 
